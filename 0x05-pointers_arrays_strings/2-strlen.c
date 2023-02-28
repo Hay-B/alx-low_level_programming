d421f999c650d1053cec87bf959d5469602ab6e5
Hay-B <hademolah1995@gmail.com>
2023-02-27 16:15:05 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: pointer
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	int t;
+
+	for (t = 0 ; s[t] != '\0'; t++)
+	{
+
+	}
+	return (t);
+}
